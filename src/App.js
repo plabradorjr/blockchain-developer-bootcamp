b199734c1a0b353ca1e2b29cf73fcdf1eca744d4
Pancho Labrador <plabradorjr@gmail.com>
2021-11-10 11:09:39 -0600

    wired bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. woot! kek
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Henlo world</h1>
+      <p>this is some paragraph text</p>
     </div>
   );
 }
