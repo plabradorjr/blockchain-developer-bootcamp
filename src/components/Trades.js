6b62b112886f935d03c7cc78b6cfbfb4792d1650
Pancho Labrador <plabradorjr@gmail.com>
2021-12-03 09:09:01 -0600

    trades done and showing, order book next

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,31 +1,58 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Spinner from './Spinner'
 import {
-    filledOrdersLoadedSelector,
-    filledOrdersSelector
-  } from '../store/selectors'
+  filledOrdersLoadedSelector,
+  filledOrdersSelector
+} from '../store/selectors'
+
+const showFilledOrders = (filledOrders) => {
+  return(
+    <tbody>
+      { filledOrders.map((order) => {
+        return(
+          <tr className={`order-${order.id}`} key={order.id}>
+            <td className="text-muted">{order.formattedTimestamp}</td>
+            <td>{order.tokenAmount}</td>
+            <td className={`text-${order.tokenPriceClass}`}>{order.tokenPrice}</td>
+          </tr>
+        )
+      }) }
+    </tbody>
+  )
+}
 
 class Trades extends Component {
   render() {
     return (
+      <div className="vertical">
         <div className="card bg-dark text-white">
-            <div className="card-header">
-                Card Title x
-            </div>
-            <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-            </div>
+          <div className="card-header">
+            Trades
+          </div>
+          <div className="card-body">
+            <table className="table table-dark table-sm small">
+              <thead>
+                <tr>
+                  <th>Time</th>
+                  <th>DAPP</th>
+                  <th>DAPP/ETH</th>
+                </tr>
+              </thead>
+              { this.props.filledOrdersLoaded ? showFilledOrders(this.props.filledOrders) : <Spinner type="table" />}
+            </table>
+          </div>
         </div>
+      </div>
     )
   }
 }
 
 function mapStateToProps(state) {
-    return {
-        filledOrdersLoaded: filledOrdersLoadedSelector(state),
-        filledOrders: filledOrdersSelector(state),
-    }
+  return {
+    filledOrdersLoaded: filledOrdersLoadedSelector(state),
+    filledOrders: filledOrdersSelector(state),
+  }
 }
 
 export default connect(mapStateToProps)(Trades)
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,6 +1,7 @@
 import { get } from 'lodash'
 import { createSelector } from 'reselect'
-import { ETHER_ADDRESS, tokens, ether } from '../helpers'
+import { ETHER_ADDRESS, tokens, ether, GREEN, RED } from '../helpers'
+import moment from 'moment'
 
 const account = state => get(state, 'web3.account')
 export const accountSelector = createSelector(account, a => a)
@@ -27,19 +28,26 @@ const filledOrders = state => get(state, 'exchange.filledOrders.data', [])
 export const filledOrdersSelector = createSelector(
   filledOrders, 
   (orders) => {
+    // Sort orders by date ascending for price comparison
+    orders = orders.sort((a,b) => a.timestamp - b.timestamp)
     // Decorate the orders
     orders = decorateFilledOrders(orders)
-
     // Sort orders by date descending for display
     orders = orders.sort((a,b) => b.timestamp -a.timestamp)
-    console.log(orders)
+    // console.log(orders)k
+    return orders
   }
 )
 
 const decorateFilledOrders = (orders) => {
+  // Track previous order to compare history
+  let previousOrder = orders[0]
   return(
     orders.map((order) => {
-      return order = decorateOrder(order)
+      order = decorateOrder(order)
+      order = decorateFilledOrder(order, previousOrder)
+      previousOrder = order // Update the previous order once it's decorated
+      return order
     })
   )
 }
@@ -65,6 +73,29 @@ const decorateOrder = (order) => {
     ...order,
     etherAmount: ether(etherAmount),
     tokenAmount: tokens(tokenAmount),
-    tokenPrice
+    tokenPrice,
+    formattedTimestamp: moment.unix(order.timestamp).format('h:mm:ss a M/D')
   })
+}
+
+const decorateFilledOrder = (order, previousOrder) => {
+  return({
+    ...order,
+    tokenPriceClass: tokenPriceClass(order.tokenPrice, order.id, previousOrder)
+  })
+}
+
+const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
+  // Show green price if only one order exists
+  if(previousOrder.id === orderId) {
+    return GREEN
+  }
+
+  // Show green price if order price higher than previous order
+  // Show red price if order price lower than previous order
+  if(previousOrder.tokenPrice <= tokenPrice) {
+    return GREEN // success
+  } else {
+    return RED // danger
+  }
 }
