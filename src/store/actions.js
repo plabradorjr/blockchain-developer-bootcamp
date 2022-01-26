d11ed17d22b8b81d0d620447ef4c1d5495237fcf
Pancho Labrador <plabradorjr@gmail.com>
2022-01-26 06:34:49 -0600

    fill orders done

--- src/store/actions.js
+++ src/store/actions.js
@@ -64,3 +64,16 @@ export function orderCancelled(order) {
   }
 }
 
+// Fill Order
+export function orderFilling() {
+  return {
+    type: 'ORDER_FILLING'
+  }
+}
+
+export function orderFilled(order) {
+  return {
+    type: 'ORDER_FILLED',
+    order
+  }
+}
